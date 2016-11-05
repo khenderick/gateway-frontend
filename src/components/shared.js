5cee1e3ddea83969e052d0348ba3b1f3299c6963
Kenneth Henderick <kenneth@ketronic.be>
2016-11-05 15:07:36 +0100

    Fix fetch polyfill for Safari

--- src/components/shared.js
+++ src/components/shared.js
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import "fetch";
 import {Container} from "aurelia-dependency-injection";
 import {Aurelia} from "aurelia-framework";
 import {BindingSignaler} from "aurelia-templating-resources";
