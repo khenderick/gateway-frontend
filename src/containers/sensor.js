45018b9e6c6090791793b419833a1ca1e955ddb1
Kenneth Henderick <kenneth@ketronic.be>
2017-03-22 19:51:33 +0100

    Better sensor value display

--- src/containers/sensor.js
+++ src/containers/sensor.js
@@ -52,18 +52,15 @@ export class Sensor extends BaseObject {
     }
 
     get temperature() {
-        if (this.rawTemperature === 95.5) {
-            return undefined;
-        }
         return this.rawTemperature;
     }
 
     set temperature(temperature) {
         this.previousTemperature = this.rawTemperature;
-        if (temperature === undefined) {
-            this.rawTemperature = 95.5;
+        if (temperature === null) {
+            temperature = undefined;
         }
-        this.rawTemperature = temperature
+        this.rawTemperature = temperature;
     }
 
     get temperatureDirection() {
@@ -74,18 +71,15 @@ export class Sensor extends BaseObject {
     }
 
     get humidity() {
-        if (this.rawHumidity === 255) {
-            return undefined;
-        }
         return this.rawHumidity;
     }
 
     set humidity(humidity) {
         this.previousHumidity = this.rawHumidity;
-        if (humidity === undefined) {
-            this.rawHumidity = 255;
+        if (humidity === null) {
+            humidity = undefined;
         }
-        this.rawHumidity = humidity
+        this.rawHumidity = humidity;
     }
 
     get humidityDirection() {
@@ -96,18 +90,15 @@ export class Sensor extends BaseObject {
     }
 
     get brightness() {
-        if (this.rawBrightness === 255) {
-            return undefined;
-        }
         return this.rawBrightness;
     }
 
     set brightness(brightness) {
         this.previousBrightness = this.rawBrightness;
-        if (brightness === undefined) {
-            this.rawBrightness = 255;
+        if (brightness === null) {
+            brightness = undefined;
         }
-        this.rawBrightness = brightness
+        this.rawBrightness = brightness;
     }
 
     get brightnessDirection() {
