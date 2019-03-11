e7308fb35f4e9a0058dd08bb7c74fc8f1b86b65b
Med Mahdi Battikh <medmahdi.battikh@gmail.com>
2019-03-11 11:18:02 +0100

    reverted all date changes

--- tests/i18n.py
+++ tests/i18n.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2019 OpenMotics BVBA
+# Copyright (C) 2017 OpenMotics BVBA
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
