79f16c6e34145da7252f04992d960e45dc7e3ecb
Med Mahdi Battikh <medmahdi.battikh@gmail.com>
2019-03-11 10:09:54 +0100

    Jest tests now use let + code format cleanup

--- tests/toolbox.test.js
+++ tests/toolbox.test.js
@@ -1,23 +1,23 @@
-import {Toolbox} from '../src/components/toolbox';
+import {
+  Toolbox
+} from '../src/components/toolbox';
 
-export class I18N_mock{
+export class I18N_mock {
 
-  static tr(string){
-    return 'Called tr '+string;
+  static tr(string) {
+    return string;
   }
 }
 
-export class Object_mock{
+export class Object_mock {
 
   constructor(attribute) {
     this.attribute = attribute;
-}
+  }
 }
 
 describe('the toolbox', () => {
-  beforeEach(() => {
-    // Not required currently, will have more in the future.
-  });
+
   it('should return time in hours', () => {
     expect(Toolbox.minutesToString(67)).toBe('01:07');
     expect(Toolbox.minutesToString(52352)).toBe('872:32');
@@ -26,18 +26,18 @@ describe('the toolbox', () => {
   });
 
   it('should remove an element from the array', () => {
-    var local_array = ['first', 'second', 'third', 'fourth'];
+    let local_array = ['first', 'second', 'third', 'fourth'];
 
     Toolbox.removeElement(local_array, 'first', 0);
     expect(local_array).toEqual(['second', 'third', 'fourth']);
 
-    var local_array = ['first'];
+    local_array = ['first'];
     Toolbox.removeElement(local_array, 'first', undefined);
     expect(local_array).toEqual([]);
   });
-  
+
   it('should check if array contains element', () => {
-    var local_array = ['first', 'second', 'third', 'fourth'];
+    let local_array = ['first', 'second', 'third', 'fourth'];
 
     expect(Toolbox.arrayHasElement(local_array, 'first', 0)).toBe(true);
     expect(Toolbox.arrayHasElement(local_array, 'fifth', 4)).toBe(false);
@@ -48,7 +48,7 @@ describe('the toolbox', () => {
   });
 
   it('should check if array contains element', () => {
-    var local_string = 'OpenMotics 2019 @~?.'
+    let local_string = 'OpenMotics 2019 @~?.'
 
     expect(Toolbox.stringContains(local_string, 'op')).toBe(false);
     expect(Toolbox.stringContains(local_string, 'Op')).toBe(true);
@@ -59,7 +59,7 @@ describe('the toolbox', () => {
   });
 
   it('should convert seconds into hh:mm:ss format', () => {
-    var returned_time = Toolbox.splitSeconds(84927)
+    let returned_time = Toolbox.splitSeconds(84927)
 
     expect(returned_time.hours).toEqual(23);
     expect(returned_time.minutes).toEqual(35);
@@ -68,29 +68,29 @@ describe('the toolbox', () => {
 
   it('should return total minutes after giving hh:mm format', () => {
     expect(Toolbox.parseTime('02:10')).toEqual(130);
-    expect(Toolbox.parseTime('54:00')).toEqual(3240);  // Case showing a scenario that should fail.
+    expect(Toolbox.parseTime('54:00')).toEqual(3240); // Case showing a scenario that should fail.
     expect(Toolbox.parseTime('54:00', '01:01')).toEqual(61);
   });
 
   it('should validate if two arrays are equal', () => {
-    var local_array1 = ['first', 'second', 'third', 'fourth'];
-    var local_array2 = ['first', 'second', 'third', 'fourth'];
+    let local_array1 = ['first', 'second', 'third', 'fourth'];
+    let local_array2 = ['first', 'second', 'third', 'fourth'];
     expect(Toolbox.arrayEquals(local_array1, local_array2)).toBe(true);
 
-    var local_array2 = ['second', 'first', 'third', 'fourth'];
+    local_array2 = ['second', 'first', 'third', 'fourth'];
     expect(Toolbox.arrayEquals(local_array1, local_array2)).toBe(false);
 
-    var local_array2 = ['first', 'second', 'third', 'fourtH'];  // Upper case character in array
+    local_array2 = ['first', 'second', 'third', 'fourtH']; // Upper case character in array
     expect(Toolbox.arrayEquals(local_array1, local_array2)).toBe(false);
-    expect(Toolbox.arrayEquals('Hello there', 'Hello there')).toBe(true);  // Does it work with strings?
+    expect(Toolbox.arrayEquals('Hello there', 'Hello there')).toBe(true); // Does it work with strings?
 
-    var local_array3 = ['first', 'second', 'third', 'fourth', 'fifth'];
+    let local_array3 = ['first', 'second', 'third', 'fourth', 'fifth'];
 
     expect(Toolbox.arrayEquals(local_array1, local_array3)).toBe(false);
-    
+
     expect(Toolbox.arrayEquals(undefined, undefined)).toBe(false);
   });
-  
+
   it('should make sure an int is within a limit', () => {
     expect(Toolbox.limit(8, 0, 10)).toEqual(8);
     expect(Toolbox.limit(Math.PI, 3, 3.15)).toBe(Math.PI);
@@ -132,9 +132,9 @@ describe('the toolbox', () => {
     expect(Toolbox.validUrl('http://h.be')).toBe(true);
     expect(Toolbox.validUrl('http://h.be!')).toBe(false);
 
-    expect(Toolbox.validUrl(undefined)).toBe(false);  // Returns fals regardless of given value
+    expect(Toolbox.validUrl(undefined)).toBe(false); // Returns fals regardless of given value
 
-    expect(Toolbox.validUrl(null)).toBe(false);  // Returns fals regardless of given value
+    expect(Toolbox.validUrl(null)).toBe(false); // Returns fals regardless of given value
   });
 
   it('should combine seperated strings', () => {
@@ -154,44 +154,52 @@ describe('the toolbox', () => {
   });
 
   it('should parse crontab', () => {
-    expect(Toolbox.parseCrontab('00 10 * * sun')).toEqual([[true, false, false, false, false, false, false], '10:00', undefined]);
+    expect(Toolbox.parseCrontab('00 10 * * sun')).toEqual([
+      [true, false, false, false, false, false, false], '10:00', undefined
+    ]);
     // Failing! : parseCrontab keeps returning undefined
   });
 
   it('should check if given data is  in given range', () => {
-    expect(Toolbox.inRanges(3, [[10, 2]])).toBe(true);
-    expect(Toolbox.inRanges(3, [[2, 10]])).toBe(true);
-    expect(Toolbox.inRanges(3, [[4, 5]])).toBe(false);
+    expect(Toolbox.inRanges(3, [
+      [10, 2]
+    ])).toBe(true);
+    expect(Toolbox.inRanges(3, [
+      [2, 10]
+    ])).toBe(true);
+    expect(Toolbox.inRanges(3, [
+      [4, 5]
+    ])).toBe(false);
 
   });
-  
+
   it('should generate Hash with the given length', () => {
-    var generated = Toolbox.generateHash(3);
+    let generated = Toolbox.generateHash(3);
     expect(generated).toBeDefined();
     expect(generated.length).toEqual(3);
 
-    var generated = Toolbox.generateHash(70);
+    generated = Toolbox.generateHash(70);
     expect(generated).toBeDefined();
     expect(generated.length).toEqual(70);
 
-    var generated = Toolbox.generateHash(257);
+    generated = Toolbox.generateHash(257);
     expect(generated).toBeDefined();
     expect(generated.length).toEqual(257);
 
-    var generated = Toolbox.generateHash('5');
+    generated = Toolbox.generateHash('5');
     expect(generated).toBeDefined();
   });
 
   it('should return the time stamp', () => {
-    var my_timestamp = new Date().getTime();
-    var toolbox_timestamp = Toolbox.getTimestamp();
+    let my_timestamp = new Date().getTime();
+    let toolbox_timestamp = Toolbox.getTimestamp();
     expect(toolbox_timestamp).toBeDefined();
     expect(toolbox_timestamp).toBeCloseTo(my_timestamp);
   });
 
   it('should sort two elements', () => {
-    expect(Toolbox.sort(3,6)).toBeDefined();
-    expect(Toolbox.sort(12,6)).toBeDefined();
+    expect(Toolbox.sort(3, 6)).toBeDefined();
+    expect(Toolbox.sort(12, 6)).toBeDefined();
   });
 
   it('should check if two objects are the same at a specific key', () => {
@@ -205,25 +213,37 @@ describe('the toolbox', () => {
     expect(Toolbox.match('ABCD', 'abcd', 0)).toBe(false);
     expect(Toolbox.match('ABCD', 'hello there!', -1)).toBe(true); // ?
 
-    var my_dict = {'id': 3, 'name': 'Dr. Gordon Freeman', 'area': 'Test chamber lambda core'};
-    var my_2_dict = {'id': 4, 'name': 'Dr. Isaac Kleiner', 'area': 'Test chamber lambda core'};
+    let my_dict = {
+      'id': 3,
+      'name': 'Dr. Gordon Freeman',
+      'area': 'Test chamber lambda core'
+    };
+    let my_2_dict = {
+      'id': 4,
+      'name': 'Dr. Isaac Kleiner',
+      'area': 'Test chamber lambda core'
+    };
     expect(Toolbox.match(my_dict, my_2_dict, 'area')).toBe(true);
 
-    var my_2_dict = {'id': 4, 'name': 'Dr. Isaac Kleiner', 'area': 'Unknown - missing'};
+    my_2_dict = {
+      'id': 4,
+      'name': 'Dr. Isaac Kleiner',
+      'area': 'Unknown - missing'
+    };
     expect(Toolbox.match(my_dict, my_2_dict, 'area')).toBe(false);
 
   });
 
   it('should return the difference between two dates', () => {
-    var date1 = new Date('September 21, 1993 03:24:00');
-    var date2 = new Date('September 21, 2018 03:24:00');
-    expect(Toolbox.dateDifference(date1,date2)).toEqual(9131);
+    let date1 = new Date('September 21, 1993 03:24:00');
+    let date2 = new Date('September 21, 2018 03:24:00');
+    expect(Toolbox.dateDifference(date1, date2)).toEqual(9131);
 
-    var date1 = new Date('December 31, 2018 00:00:00');
-    var date2 = new Date('January 01, 2019 00:00:00');
-    expect(Toolbox.dateDifference(date1,date2)).toEqual(1);
+    date1 = new Date('December 31, 2018 00:00:00');
+    date2 = new Date('January 01, 2019 00:00:00');
+    expect(Toolbox.dateDifference(date1, date2)).toEqual(1);
 
-    expect(() => Toolbox.dateDifference(null,date2)).toThrow(TypeError);
+    expect(() => Toolbox.dateDifference(null, date2)).toThrow(TypeError);
   });
 
   it('should parse string dates', () => {
@@ -233,11 +253,12 @@ describe('the toolbox', () => {
   });
 
   it('should format bytes into Gibibytes', () => {
-    var result = Toolbox.formatBytes(1073741824 , I18N_mock);
+    let result = Toolbox.formatBytes(1073741824, I18N_mock);
     expect(result).toBeDefined();
-    var extracted_number = parseFloat(result.match(/\d+.\d{2}/)[0]);
+    expect(result).toEqual('1.00 generic.units.gib');
+    let extracted_number = parseFloat(result.match(/\d+.\d{2}/)[0]);
     expect(extracted_number).toBeCloseTo(1.0);
-    var extracted_unit = result.match(/generic.*/)[0];
+    let extracted_unit = result.match(/generic.*/)[0];
     expect(extracted_unit).toEqual('generic.units.gib');
   });
 
@@ -246,7 +267,7 @@ describe('the toolbox', () => {
     expect(Toolbox.isDate('2015-03-25 12:00:00')).toBe(false);
     expect(Toolbox.isDate('December 12, 2012 12:12')).toBe(false);
     expect(Toolbox.isDate(undefined)).toBe(false);
-    expect(() =>Toolbox.isDate(null)).toThrow(TypeError);
+    expect(() => Toolbox.isDate(null)).toThrow(TypeError);
   });
 
   it('should convert system64 to percent', () => {
@@ -276,9 +297,9 @@ describe('the toolbox', () => {
   });
 
   it('should format date', () => {
-    var now = new Date();
+    let now = new Date();
     expect(Toolbox.formatDate(now, 'MM/dd/yyyy')).toBeDefined();
-    expect(Math.trunc(Toolbox.formatDate(now, 'MM'))).toEqual(now.getMonth()+1);
+    expect(Math.trunc(Toolbox.formatDate(now, 'MM'))).toEqual(now.getMonth() + 1);
     expect(Math.trunc(Toolbox.formatDate(now, 'dd'))).toEqual(now.getDate());
     expect(Math.trunc(Toolbox.formatDate(now, 'yyyy'))).toEqual(now.getFullYear());
   });
@@ -286,17 +307,17 @@ describe('the toolbox', () => {
   it('should get device view port', () => {
     expect(Toolbox.getDeviceViewport()).not.toBeDefined();
   });
-  
+
   it('should ensure default value', () => {
-    var my_object = Object_mock;
+    let my_object = Object_mock;
     Toolbox.ensureDefault(my_object, 'attribute', 'default');
     expect(my_object.attribute).toEqual('default');
 
   });
 
   it('should format range between two dates', () => {
-    var date1 = new Date('October 21, 2018 03:24:00');
-    var date2 = new Date('September 21, 2018 03:24:00');
+    let date1 = new Date('October 21, 2018 03:24:00');
+    let date2 = new Date('September 21, 2018 03:24:00');
     expect(Toolbox.formatDateRange(date1, date2, 'dd/MM/yyyy', I18N_mock)).toEqual('21/10/2018 - 21/09/2018');
     expect(Toolbox.formatDateRange(date1, date2, 'yyyy', I18N_mock)).toEqual('2018 - 2018');
   });
@@ -309,7 +330,7 @@ describe('the toolbox', () => {
 
   it('should shorten lists', () => {
     expect(Toolbox.shortenList(['one', 'two', 'three', 'four'], 4, I18N_mock)).toEqual('one, two, three, four');
-    expect(Toolbox.shortenList(['9g06EJb8rMEqk98b88kF', 'mURAEpkGVU3BFbLDqW13', 'fpv9Gc9sfml0lZdjcmmo', 'XDkqIVhBHtqtKfPLUeFc'], 50, I18N_mock)).toEqual('9g06EJb8rMEqk98b88kFCalled tr generic.andxmore');
+    expect(Toolbox.shortenList(['9g06EJb8rMEqk98b88kF', 'mURAEpkGVU3BFbLDqW13', 'fpv9Gc9sfml0lZdjcmmo', 'XDkqIVhBHtqtKfPLUeFc'], 50, I18N_mock)).toEqual('9g06EJb8rMEqk98b88kFgeneric.andxmore');
 
     expect(Toolbox.shortenList(['one', 'two', 'three', 'four'], undefined, I18N_mock)).toEqual('one, two, three, four');
 
@@ -322,14 +343,14 @@ describe('the toolbox', () => {
 
   it('should sleep for the given time', () => {
 
-    var MySleepyPromise = Toolbox.sleep(10000);
+    let MySleepyPromise = Toolbox.sleep(10000);
 
 
     expect(MySleepyPromise).toBeInstanceOf(Promise);
-    
+
     MySleepyPromise.then(callback => {
 
-      expect(callback).not.toHaveBeenCalledWith(10000);
+      expect(callback).toHaveBeenCalledTimes(0);
       jest.runAllTimers();
       expect(callback).toBeCalled();
       expect(callback).toHaveBeenCalledTimes(1);
