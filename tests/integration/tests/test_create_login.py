7f65123639aa7ee5d3bbb7a2c8372f699b96d8e0
Med Mahdi Battikh <medmahdi.battikh@gmail.com>
2019-05-16 11:13:34 +0200

    now using assertion to validate if max retries is reached

--- tests/integration/tests/test_create_login.py
+++ tests/integration/tests/test_create_login.py
@@ -10,10 +10,10 @@ class TestCreateLogin(unittest.TestCase):
     helper = None
 
     def test_the_title_is_openmotics(self):
-
-        for attempt in xrange(MAX_ALLOWED_RETRIES):
+        attempt = 0
+        while attempt < MAX_ALLOWED_RETRIES:
             try:
-                self.driver.get("https://{0}/".format(self.helper.testee_ip))
+                self.driver.get('https://{0}/'.format(self.helper.testee_ip))
                 self.driver.implicitly_wait(self.helper.global_timeout)  # Wait for the landing page to finish rendering
 
                 elem = self.helper.find_element_where("id=login.create", self.driver)
@@ -22,23 +22,23 @@ class TestCreateLogin(unittest.TestCase):
                 token = self.helper.get_new_tester_token()
                 self.helper.enter_testee_authorized_mode(token, timeout=None)
 
-                if "OpenMotics" not in self.driver.title:
+                if 'OpenMotics' not in self.driver.title:
                     raise Exception('Title validation failed')
 
                 elem = self.helper.find_element_where('id=create.username', self.driver)
-                elem.send_keys("automatedusername")
+                elem.send_keys('automatedusername')
                 elem = self.helper.find_element_where('id=create.password', self.driver)
-                elem.send_keys("automatedpassword")
+                elem.send_keys('automatedpassword')
                 elem = self.helper.find_element_where('id=create.confirmpassword', self.driver)
-                elem.send_keys("automatedpassword")
+                elem.send_keys('automatedpassword')
                 elem = self.helper.find_element_where('id=create.create', self.driver)
                 elem.click()
                 elem = self.helper.find_element_where('id=create.havelogin', self.driver)
                 elem.click()
                 elem = self.helper.find_element_where('id=login.username', self.driver)
-                elem.send_keys("automatedusername")
+                elem.send_keys('automatedusername')
                 elem = self.helper.find_element_where('id=login.password', self.driver)
-                elem.send_keys("automatedpassword")
+                elem.send_keys('automatedpassword')
                 elem = self.helper.find_element_where('id=login.signin', self.driver)
                 elem.click()
                 elem = self.helper.find_element_where('id=login.acceptterms', self.driver)
@@ -46,12 +46,12 @@ class TestCreateLogin(unittest.TestCase):
                 elem = self.helper.find_element_where('id=login.signin', self.driver)
                 elem.click()
 
-                if "OpenMotics" not in self.driver.title:
+                if 'OpenMotics' not in self.driver.title:
                     raise Exception('Title validation failed')
                 break
             except Exception as ex:  # Should be NoSuchElementException, but will retry on any failure
-                if attempt+1 == MAX_ALLOWED_RETRIES:
-                    self.fail("Max retries reached. Error: {0}".format(ex))
+                attempt += 1
                 time.sleep(2)
-        self.assertTrue(attempt < MAX_ALLOWED_RETRIES)  # This will be always True if code reaches here, using it to pass the test.
+
         self.driver.quit()
+        self.assertTrue(attempt < MAX_ALLOWED_RETRIES, 'Maximum retries reached.')
