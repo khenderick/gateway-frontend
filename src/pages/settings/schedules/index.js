da9beaf50412f9b55a61e14a5e6b36d82fc30779
Oleg Mysin <itolegmysin@gmail.com>
2019-11-26 14:31:19 +0300

    Schedules page refactored

--- src/pages/settings/schedules/index.js
+++ src/pages/settings/schedules/index.js
@@ -16,12 +16,12 @@
  */
 import {computedFrom, inject, Factory} from 'aurelia-framework';
 import {DialogService} from 'aurelia-dialog';
-import {Base} from '../../resources/base';
-import {Refresher} from '../../components/refresher';
-import {Toolbox} from '../../components/toolbox';
-import {Logger} from '../../components/logger';
-import {Schedule} from '../../containers/schedule';
-import {ConfigureScheduleWizard} from '../../wizards/configureschedule/index';
+import {Base} from 'resources/base';
+import {Refresher} from 'components/refresher';
+import {Toolbox} from 'components/toolbox';
+import {Logger} from 'components/logger';
+import {Schedule} from 'containers/schedule';
+import {ConfigureScheduleWizard} from 'wizards/configureschedule/index';
 
 @inject(DialogService, Factory.of(Schedule))
 export class Schedules extends Base {
