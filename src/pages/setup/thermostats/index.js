e6360279e0932d231ace037dbde0300376b09d44
Daiderd Jordan <djordan@openmotics.com>
2021-07-06 16:25:12 +0200

    simplify sensor temperature loading

--- src/pages/setup/thermostats/index.js
+++ src/pages/setup/thermostats/index.js
@@ -273,31 +273,13 @@ export class Thermostats extends Base {
 
     async loadSensors() {
         try {
-            let [configuration, temperature] = await Promise.all([this.api.getSensorConfigurations(), this.api.getThermostatUnits()]);
-            temperature = temperature.data.reduce((prev, { configuration: { heating, cooling }, status }) => {
-                let temperatures = {};
-                if (heating && heating.hasOwnProperty('sensor_id')) {
-                    temperatures = {
-                        ...prev,
-                        [heating.sensor_id]: status.actual_temperature,
-                    };
-                }
-                if (cooling && cooling.hasOwnProperty('sensor_id')) {
-                    temperatures = {
-                        ...prev,
-                        [cooling.sensor_id]: status.actual_temperature,
-                    };
-                }
-                return temperatures;
-            }, {});
+            let [configuration, temperature] = await Promise.all([this.api.getSensorConfigurations(), this.api.getSensorTemperatureStatus()]);
             Toolbox.crossfiller(configuration.config, this.sensors, 'id', (id) => {
                 let sensor = this.sensorFactory(id);
+                sensor.temperature = temperature.status[sensor.id];
                 this.sensorMap[id] = sensor;
                 return sensor;
             });
-            for (let sensor of this.sensors) {
-                sensor.temperature = temperature[sensor.id];
-            }
             this.sensors.sort((a, b) => {
                 return a.id > b.id ? 1 : -1;
             });
